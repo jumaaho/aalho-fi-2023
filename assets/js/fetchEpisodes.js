5d655002454de8c80e82c5932898c537ff46f88f
Jukka Ahola <Jukka@MacBook-Pro.local>
2023-07-10 17:19:30 +0300

    formatted podcast feed

--- assets/js/fetchEpisodes.js
+++ assets/js/fetchEpisodes.js
@@ -2,13 +2,12 @@ fetch('/.netlify/functions/fetchPodcasts')
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
-    console.log(data);
     let items = data.querySelectorAll("item");
     let html = `<ul>`;
-    for(let i=0; i<3; i++) {
+    for(let i = 0; i < items.length; i++) {
         let title = items[i].querySelector("title").textContent;
-        let pubDate = items[i].querySelector("pubDate").textContent;
-        html += `<li><b>${title}</b> <i>${pubDate}</i></li>`;
+        let link = items[i].querySelector("link").textContent;
+        html += `<li><a href="${link}">${title}</a></li>`;
     }
     html += `</ul>`;
     document.getElementById('rss-feed-display').innerHTML = html;
