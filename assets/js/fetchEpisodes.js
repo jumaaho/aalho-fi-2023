687259736b9e53f49a2a236ed86818a149488206
Jukka Ahola <Jukka@MacBook-Pro.local>
2023-07-10 17:21:45 +0300

    formatted podcast feed more

--- assets/js/fetchEpisodes.js
+++ assets/js/fetchEpisodes.js
@@ -4,7 +4,7 @@ fetch('/.netlify/functions/fetchPodcasts')
   .then(data => {
     let items = data.querySelectorAll("item");
     let html = `<ul>`;
-    for(let i = 0; i < items.length; i++) {
+    for(let i = 0; i < 3; i++) {
         let title = items[i].querySelector("title").textContent;
         let link = items[i].querySelector("link").textContent;
         html += `<li><a href="${link}">${title}</a></li>`;
