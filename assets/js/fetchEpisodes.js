e1f9d7d6ed21c057a8c9e191a4175c8db5ee7a2d
Jukka Ahola <Jukka@MacBook-Pro.local>
2023-07-10 16:03:56 +0300

    Initial commit

---
+++ assets/js/fetchEpisodes.js
@@ -0,0 +1,15 @@
+fetch('https://kertojanaani.fi/feed/podcast')
+.then(response => response.text())
+.then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+.then(data => {
+    console.log(data);
+    let items = data.querySelectorAll("item");
+    let html = `<ul>`;
+    for(let i=0; i<3; i++) {
+        let title = items[i].querySelector("title").textContent;
+        let pubDate = items[i].querySelector("pubDate").textContent;
+        html += `<li><b>${title}</b> <i>${pubDate}</i></li>`;
+    }
+    html += `</ul>`;
+    document.getElementById('rss-feed-display').innerHTML = html;
+});
