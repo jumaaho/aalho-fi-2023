c6014427dda1b4764381355d0e6ead2def17cb3a
Jukka Aalho <jumaaho@gmail.com>
2024-02-02 08:46:29 +0200

    lisätty podcast-feedi Markkinointiperunalle

--- assets/js/fetchEpisodes.js
+++ assets/js/fetchEpisodes.js
@@ -1,4 +1,5 @@
-fetch('/.netlify/functions/fetchPodcasts')
+// Fetch and display the first podcast
+fetch('/.netlify/functions/fetchPodcasts?podcast=kertojanaani')
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
@@ -10,5 +11,21 @@ fetch('/.netlify/functions/fetchPodcasts')
         html += `<li><a href="${link}">${title}</a></li>`;
     }
     html += `</ul>`;
-    document.getElementById('rss-feed-display').innerHTML = html;
+    document.getElementById('rss-feed-display-kertojanaani').innerHTML = html;
+});
+
+// Fetch and display the second podcast
+fetch('/.netlify/functions/fetchPodcasts?podcast=markkinointiperuna')
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    let items = data.querySelectorAll("item");
+    let html = `<ul>`;
+    for(let i = 0; i < 3; i++) {
+        let title = items[i].querySelector("title").textContent;
+        let link = items[i].querySelector("link").textContent;
+        html += `<li><a href="${link}">${title}</a></li>`;
+    }
+    html += `</ul>`;
+    document.getElementById('rss-feed-display-markkinointiperuna').innerHTML = html;
 });
--- netlify/functions/fetchPodcasts.js
+++ netlify/functions/fetchPodcasts.js
@@ -1,12 +1,24 @@
 const fetch = (...args) =>
   import('node-fetch').then(({default: fetch}) => fetch(...args));
-  
-exports.handler = async function(event, context) {
+
+exports.handler = async function(event) {
+  const podcast = event.queryStringParameters.podcast; // Get podcast identifier from query parameters
+  let feedURL = '';
+
+  // Define the feed URL based on the podcast query parameter
+  if (podcast === 'kertojanaani') {
+    feedURL = 'https://kertojanaani.fi/feed/podcast';
+  } else if (podcast === 'markkinointiperuna') {
+    feedURL = 'https://aalhomedia.fi/feed/podcast'; // Example URL, replace with the actual feed URL
+  } else {
+    return { statusCode: 400, body: 'Invalid podcast identifier' };
+  }
+
   try {
-    const response = await fetch('https://kertojanaani.fi/feed/podcast');
+    const response = await fetch(feedURL);
     const data = await response.text();
     return { statusCode: 200, body: data };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
   }
-}
+};
